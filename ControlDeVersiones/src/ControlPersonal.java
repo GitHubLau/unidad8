922b0821e5bbf8a359a7372b52f3dfc098495771
Lau <lau.priego@gmail.com>
2015-05-03 20:56:48 +0200

    Clase persona implementada

--- ControlDeVersiones/src/ControlPersonal.java
+++ ControlDeVersiones/src/ControlPersonal.java
@@ -1,9 +1,79 @@
+import java.util.ArrayList;
 
-public class ControlPersonal {
+/**
+ * @author LAURA MARTINEZ PRIEGO 1oDAM
+ */
+public class ControlPersonal 
+{
 
-	public static void main(String[] args) {
-		// TODO Apéndice de método generado automáticamente
-
+	private static ArrayList<Persona> personas;
+	
+	public ControlPersonal()
+	{}
+	
+	/**
+	 * Crea asignaturas (mínimo 3), crea profesores (mínimo 3), crea
+		alumnos (mínimo 3), añádelas al ArrayList y muestra el listado de los alumnos ordenado por
+		nombre, el listado de alumnos por nombre de asignatura, listado de profesores ordenado por
+		dni, listado de alumnos ordenado por nota y por último el listado de las notas que le ha
+		asignado la universidad a cada profesor, es decir, el listado de profesores ordenado por nota.
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		Asignatura programacion = new Asignatura("programacion");
+			
+		Asignatura basesDatos = new Asignatura("bases de datos");
+		Asignatura sistemas = new Asignatura("sistemas");
+		
+		Profesor dProg = new Profesor ("pepe", "123456789");
+			anyadirPersonas (dProg);
+		Profesor dBD = new Profesor ("carlos", "987654321");
+			anyadirPersonas (dBD);
+		Profesor dsist = new Profesor ("santi", "123454321");
+			anyadirPersonas (dsist);
+		
+		Alumno alum1 = new Alumno ("claudia", "098765432");
+			anyadirPersonas (alum1);
+		Alumno alum2 = new Alumno ("pepita", "234567890");
+			anyadirPersonas (alum2);
+		Alumno alum3 = new Alumno ("claudia", "098767890");
+			anyadirPersonas (alum3);
+	}
+	
+	
+	/**
+	 * : añade la persona al ArrayList
+	 * @param p
+	 */
+	public static void anyadirPersonas(Persona p)
+	{
+		personas.add(p);
+	}
+	
+	
+	/**
+	 * : ordena el ArrayList por dni
+	 */
+	public void ordenarPorDNI()
+	{
+		
+	}
+	
+	/**
+	 * : ordena el ArrayList por nombre
+	 */
+	public void ordenarPorOrdenAlfabetico()
+	{
+		
+	}
+	
+	/**
+	 * : método para mostrar por pantalla los listados correspondientes
+	 */
+	public void mostrarListado()
+	{
+		
 	}
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # unidad8
 ejercicio 2 unidad 8, control de versiones
+
+Por temas personales no he dispuesto de suficiente tiempo para poder implementar debidamente las clases, he priorizado el trabajar sobre GitHuB y la entrega del pdf por ser el tema de la unidad, lo lamento.
